01afda58d1edf2f6089adf8dbe28e77582081bab
vinicius.silva <vinicius.silva@onlineapp.com.br>
2019-09-30 14:06:16 -0300

    Solução

--- Reflection/src/Challenge/Program.cs
+++ Reflection/src/Challenge/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Text;
 
 namespace Challenge
@@ -9,16 +11,31 @@ namespace Challenge
      * Escrever uma nova opção na classe Escrever para receber qualquer objeto e retorne todas seus valores e propriedades.
      * Escrever um Metodo na class Program que execute o objeto Escrever logar de forma generica.
      */
-    
+
     class Program
     {
         static void Main(string[] args)
         {
-            var nota = new NotaFiscal { Numero = 1210, Chave = "NFE1023023012302103012202" };
-            var escrever = new Escrever();
-            escrever.Log(nota);
-            Console.ReadKey();
+            var objToLog = new List<ClassePai>();
 
+            var objP1 = new ClassePai { NomePai = "Papai", Filhos = new List<ClasseFilho>() };
+            var objF1 = new ClasseFilho { NomeFilho = "Filho 1" };
+            var objF2 = new ClasseFilho { NomeFilho = "Filho 2" };
+            objP1.Filhos.Add(objF1);
+            objP1.Filhos.Add(objF2);
+
+            var objP2 = new ClassePai { NomePai = "Papai 2", Filhos = new List<ClasseFilho>() };
+            var objF3 = new ClasseFilho { NomeFilho = "Filho 3" };
+            var objF4 = new ClasseFilho { NomeFilho = "Filho 4" };
+            objP2.Filhos.Add(objF3);
+            objP2.Filhos.Add(objF4);
+
+            objToLog.Add(objP1);
+            objToLog.Add(objP2);
+
+            var escrever = new Escrever();
+            escrever.LogGenerico(objToLog);
+            Console.ReadKey();
         }
 
 
@@ -31,6 +48,53 @@ namespace Challenge
                 dados.AppendLine($"{nameof(nota.Numero)}: {nota.Numero}");
                 dados.AppendLine($"{nameof(nota.Chave)}: {nota.Chave}");
                 Console.WriteLine(dados.ToString());
+            }
+            public void LogEscreveLinha(string linha)
+            {
+                Console.WriteLine(linha);
+            }
+            public void LogGenerico<T>(List<T> objParam)
+            {
+                foreach (var obj in objParam)
+                {
+                    var props = obj.GetType().GetProperties();
+                    foreach (var prop in props)
+                    {
+                        var value = prop.GetValue(obj);
+                        if (value is IList && value.GetType().IsGenericType)
+                        {
+                            foreach (var item in (IEnumerable)value)
+                            {
+                                LogGenerico(item);
+                            }
+                        }
+                        else
+                        {
+                            var linha = prop.Name.ToString() + ":" + prop.GetValue(obj).ToString();
+                            LogEscreveLinha(linha);
+                        }
+                    }
+                }
+            }
+            public void LogGenerico<T>(T obj)
+            {
+                var props = obj.GetType().GetProperties();
+                foreach (var prop in props)
+                {
+                    var value = prop.GetValue(obj);
+                    if (value is IList && value.GetType().IsGenericType)
+                    {
+                        foreach(var item in (IEnumerable)value)
+                        {
+                            LogGenerico(item);
+                        }
+                    }
+                    else
+                    {
+                        var linha = prop.Name.ToString() + ":" + prop.GetValue(obj).ToString();
+                        LogEscreveLinha(linha);
+                    }
+                }
             }
         }
 
@@ -59,6 +123,16 @@ namespace Challenge
             public decimal ValorTotal { get; set; }
             public decimal FreteTotal { get; set; }
 
+        }
+
+        public class ClassePai
+        {
+            public string NomePai { get; set; }
+            public List<ClasseFilho> Filhos { get; set; }
+        }
+        public class ClasseFilho
+        {
+            public string NomeFilho { get; set; }
         }
     }
 }
